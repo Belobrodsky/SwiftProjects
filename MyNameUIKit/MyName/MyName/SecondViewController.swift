6ca9fef2d10af138a2fb0d6cdb4438f37b2aa3a2
Vladimir Belobrodsky <belobrodsky@yandex.ru>
2022-01-31 15:46:59 +0300

    Add second ViewController and func Alert on second scene

---
+++ MyNameUIKit/MyName/MyName/SecondViewController.swift
@@ -0,0 +1,57 @@
+//
+//  SecondViewController.swift
+//  MyName
+//
+//  Created by Владимир Белобродский on 31.01.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//
+//
+//        // Do any additional setup after loading the view.
+//    }
+@IBAction func ShowMessageOrange() -> Void {
+    
+    print("Orange")}
+
+    
+    @IBAction func ShowAlert(){
+        let alertController = UIAlertController(title: "Привет, мир", message: "ОО да, детка, ты работаешь хорошо", preferredStyle: UIAlertController.Style.alert)
+        
+        let actionOK = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { _ in print("Ok buton pressed")
+            
+        }
+        let actionCancel = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel) { _ in print("Cancel buton pressed")
+            
+        }
+        
+        let actionCancel2 = UIAlertAction(title: "Cancel2", style: UIAlertAction.Style.destructive) { _ in print("Cancel2 (Dectructive) buton pressed")
+            
+        }
+        
+        
+        alertController.addAction(actionOK)
+        alertController.addAction(actionCancel)
+        alertController.addAction(actionCancel2)
+        
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
