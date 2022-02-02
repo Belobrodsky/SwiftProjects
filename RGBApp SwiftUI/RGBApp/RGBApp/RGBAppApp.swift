a8d548ec25f1f8f2212e4f0983eea341b8cb6615
Vladimir Belobrodsky <belobrodsky@yandex.ru>
2022-02-02 15:58:35 +0300

    First SwiftUI APp

---
+++ RGBApp SwiftUI/RGBApp/RGBApp/RGBAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBAppApp.swift
+//  RGBApp
+//
+//  Created by Владимир Белобродский on 02.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
